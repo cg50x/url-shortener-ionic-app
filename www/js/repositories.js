fe365295f4327005b336339e9cb98b42455f3f54
Chris Guevara <chris@homdna.com>
2017-01-06 23:56:39 -0800

    Use localforage

--- www/js/repositories.js
+++ www/js/repositories.js
@@ -19,15 +19,16 @@ function ($window, $q, ShortURLAPI) {
 		// Use the short url repository to retrieve the specific short url
 		// if information doesn't exist, use the API
 		// if information is expired, use the API
-		var cachedItem = _getCacheItem(shortURLID);
-		if (cachedItem) {
-			return $q.resolve(cachedItem);
-		} else {
-			return ShortURLAPI.getShortURL(shortURLID).then(function (shortURL) {
-				_setCacheItem(shortURL['short_url_id'], shortURL);
-				return shortURL;
-			});
-		}
+		return _getCacheItem(shortURLID).then(function (cachedItem) {
+			if (cachedItem) {
+				return cachedItem;
+			} else {
+				return ShortURLAPI.getShortURL(shortURLID).then(function (shortURL) {
+					_setCacheItem(shortURL['short_url_id'], shortURL);
+					return shortURL;
+				});
+			}
+		});
 	}
 
 	function createShortURL (longURL) {
@@ -37,21 +38,26 @@ function ($window, $q, ShortURLAPI) {
 		});
 	}
 
-	// TODO use localForage for caching
+	// Returns a promise
 	function _setCacheItem (cacheKey, shortURL) {
 		var objectToCache = shortURL;
-		var jsonToCache = JSON.stringify(objectToCache);
-		return $window.localStorage.setItem(CACHE_KEY_PREFIX + cacheKey, jsonToCache);
+		return $q.resolve(ShortURLStore.setItem(cacheKey, objectToCache));
 	}
 
+	// Returns a promise
 	function _getCacheItem (cacheKey, shortURL) {
-		var jsonString = $window.localStorage.getItem(CACHE_KEY_PREFIX + cacheKey, shortURL);
-		return JSON.parse(jsonString);
+		return $q.resolve(ShortURLStore.getItem(cacheKey, shortURL));
 	}
 
-	// Because all repositories will be sharing the same cache - localStorage,
-	// need to add a prefix to all cache keys.
-	var CACHE_KEY_PREFIX = 'shortURLs/';
+	// Initializing the persistent cache
+	var ShortURLStore = localforage.createInstance({
+		driver: [
+			localforage.LOCALSTORAGE,
+			localforage.INDEXEDDB,
+			localforage.WEBSQL
+		],
+		name: 'ShortURLs'
+	});
 
 	return {
 		getShortURLs: getShortURLs,
