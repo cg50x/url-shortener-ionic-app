138cfe765f21b0993513f38417d559b89d5abc02
Chris Guevara <chris@homdna.com>
2017-01-11 23:00:09 -0800

    Add Babel; add src folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,43 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+var runSequence = require('run-sequence');
 
 var paths = {
+  src: './src',
+  lib: './src/lib',
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['babel', 'sass']);
+
+gulp.task('build-www', function (done) {
+  runSequence('copy-to-www', 'babel', 'sass', done);
+});
+
+gulp.task('copy-to-www', function () {
+  return gulp.src([
+    paths.src + '/**/*',
+    '!' + paths.src + '/**/*.scss',
+    '!' + paths.styles])
+    .pipe(gulp.dest('www'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('www'));
+});
+
+gulp.task('babel', function () {
+  return gulp.src([
+    paths.src + '/**/*.js',
+    '!' + paths.lib + '/**/*'])
+    .pipe(plumber())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('www'));
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -26,9 +57,13 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('watch', ['build-www'], function() {
+  gulp.watch(paths.src + '/**/*.html', ['html']);
+  gulp.watch(paths.src + '/**/*.js', ['babel']);
+  gulp.watch(paths.src + '/**/*.scss', ['sass']);
 });
+
+gulp.task('serve:before', ['watch']);
 
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
